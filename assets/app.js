3f8c1147b8559f39674d7f0bcceaf6f67a6154ea
Marcel <marcelx@protonmail.com>
2020-06-27 09:15:13 -0300

    Versão 27do6
    Vers. Hello World
    
    É possível verificar o dado referente aos bairros mediante a seleção. Também há um botão de refresh.

---
+++ assets/app.js
@@ -0,0 +1,114 @@
+var valor = document.getElementById("bairro").value;
+
+
+function myFunction() {
+    document.getElementById("resultado").innerHTML = `A estimativa de vida em seu bairro é de ${valor} anos`;
+    document.getElementById("texto").innerHTML = `Em Cidade Tiradentes, a média de vida é de 57 anos, enquanto que em Moema, por exemplo, a expectativa chega aos 80 anos.
+    `
+}
+
+
+  // function declara() {
+    // alert(`${x} anoss`);
+    // document.getElementById("main").innerHTML = "Em Cidade Tiradentes, a média de vida é de 57 anos, enquanto que em Moema, por exemplo, a expectativa chega aos 80 anos.";
+
+ //  };
+
+
+
+// Água Rasa|73
+// Alto de Pinheiros|79
+// Anhangüera|58
+// Aricanduva|70
+// Artur Alvim|68
+// Barra Funda|73
+// Bela Vista|73
+// Belém|89
+// Bom Retiro|68
+// Brás	|65
+// Brasilândia|60
+// Butantã|74
+// Cachoeirinha|63
+// Cambuci|71
+// Campo Belo|76
+// Campo Grande|72
+// Campo Limpo|63
+// Cangaíba|67
+// Capão Redondo|60
+// Carrão|73
+// Casa Verde|71
+// Cidade Ademar|64
+// Cidade Dutra|65
+// Cidade Líder|66
+// Cidade Tiradentes|57
+// Consolação|79
+// Cursino|72
+// Ermelino Matarazzo|65
+// Freguesia do Ó|69
+// Grajaú|58
+// Guaianases|62
+// Iguatemi|59
+// Ipiranga|73
+// Itaim Bibi|78
+// Itaim Paulista|61
+// Itaquera|64
+// Jabaquara|70
+// Jaçanã|67
+// Jaguara|68
+// Jaguaré|67
+// Jaraguá|61
+// Jardim Ângela|58
+// Jardim Helena|62
+// Jardim Paulista|79
+// Jardim São Luís|61
+// José Bonifácio|63
+// Lajeado|60
+// Lapa|78
+// Liberdade|74
+// Limão|70
+// Mandaqui|72
+// Marsilac|57
+// Moema|80
+// Moóca|76
+// Morumbi|73
+// Parelheiros|59
+// Pari|66
+// Parque do Carmo|63
+// Pedreira|61
+// Penha|71
+// Perdizes|78
+// Perus|61
+// Pinheiros|78
+// Pirituba|68
+// Ponte Rasa|67
+// Raposo Tavares|64
+// República|66
+// Rio Pequeno|69
+// Sacomã|67
+// Santa Cecília|74
+// Santana|75
+// Santo Amaro|78
+// São Domingos|70
+// São Lucas|71
+// São Mateus|65
+// São Miguel|67
+// São Rafael|58
+// Sapopemba|63
+// Saúde|77
+// Sé|64
+// Socorro|74
+// Tatuapé|76
+// Tremembé|66
+// Tucuruvi|74
+// Vila Andrade|63
+// Vila Curuçá|63
+// Vila Formosa|72
+// Vila Guilherme|72
+// Vila Jacuí|63
+// Vila Leopoldina|74
+// Vila Maria|66
+// Vila Mariana|78
+// Vila Matilde|71
+// Vila Medeiros|68
+// Vila Prudente|71
+// Vila Sônia|68
